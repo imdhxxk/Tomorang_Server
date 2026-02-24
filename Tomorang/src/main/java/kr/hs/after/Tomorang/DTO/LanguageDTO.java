a9fe10e98c1071b3002bc94afa447913da2011f0
임동혁 <barinlim77@gmail.com>
2026-02-24 10:40:17 +0900

    Add files via upload

---
+++ Tomorang/src/main/java/kr/hs/after/Tomorang/DTO/LanguageDTO.java
@@ -0,0 +1,13 @@
+package kr.hs.after.Tomorang.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LanguageDTO {
+    private String lang;
+    private String level;
+}
